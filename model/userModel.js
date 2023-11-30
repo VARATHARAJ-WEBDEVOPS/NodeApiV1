7fd9558abb7ce702b255da1168006b054fd2caf3
varatharaj <varathan2512002@gmail.com>
2023-11-30 11:51:19 +0530

    Email is removed in userModel

--- model/userModel.js
+++ model/userModel.js
@@ -10,10 +10,6 @@ const userShcema = mongoose.Schema(
             type: Number,
             required: true,
         },
-        email: {
-            type: String,
-            required: true,
-        },
         profileImage: {
             type: String,
             required: false
